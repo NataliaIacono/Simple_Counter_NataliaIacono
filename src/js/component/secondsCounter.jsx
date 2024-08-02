38945ddb2b4f5fa98e55352d663972415fa54cd6
Natalia Iacono <natalia.iacono.pr@gmail.com>
2024-08-02 18:51:43 +0200

    ok

--- src/js/component/secondsCounter.jsx
+++ src/js/component/secondsCounter.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import '../../styles/secondsCounter.css';
 
 const SecondsCounter = ({ uno, dos, tres, cuatro, cinco, seis }) => {
     return (
-        <div className="container-fluid d-flex justify-content-around d-flex fs-1 mt-5 bg-dark">
-            <div className="icon">
+        <div className="container row">
+            <div className="icono col-lg-2 col-sm-12">
                 <i className="fa fa-clock text-white"></i>
             </div>
-            <div className="seis text-white">{seis % 10}</div>
-            <div className="cinco text-white">{cinco % 10}</div>
-            <div className="cuatro text-white">{cuatro % 10}</div>
-            <div className="tres text-white">{tres % 10}</div>
-            <div className="dos text-white">{dos % 10}</div>
-            <div className="uno text-white">{uno % 10}</div>
+            <div className="seis col text-white">{seis % 10}</div>
+            <div className="cinco col text-white">{cinco % 10}</div>
+            <div className="cuatro col text-white">{cuatro % 10}</div>
+            <div className="tres col text-white">{tres % 10}</div>
+            <div className="dos col text-white">{dos % 10}</div>
+            <div className="uno col text-white">{uno % 10}</div>
         </div>
     );
 };
