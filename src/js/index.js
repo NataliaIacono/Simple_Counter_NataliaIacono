e36aaf4d8e28e6890b1b595b187941863efa6265
Natalia Iacono <natalia.iacono.pr@gmail.com>
2024-08-04 13:50:05 +0200

    evento onload ok

--- src/js/index.js
+++ src/js/index.js
@@ -7,18 +7,20 @@ import SecondsCounter from './component/secondsCounter.jsx';
 import '../styles/index.css';
 let containerApp = ReactDOM.createRoot(document.getElementById('app'));
 let contador = 0;
-setInterval(() => {
-    const seis = Math.floor(contador / 10000);
-    const cinco = Math.floor(contador / 10000);
-    const cuatro = Math.floor(contador / 1000);
-    const tres = Math.floor(contador / 100);
-    const dos = Math.floor(contador / 10);
-    const uno = Math.floor(contador / 1);
+const startCounter = () => {
+    setInterval(() => {
+        const seis = Math.floor(contador / 10000);
+        const cinco = Math.floor(contador / 10000);
+        const cuatro = Math.floor(contador / 1000);
+        const tres = Math.floor(contador / 100);
+        const dos = Math.floor(contador / 10);
+        const uno = Math.floor(contador / 1);
 
-    if (contador < 300) {
-        contador++;
-        containerApp.render(<SecondsCounter uno={uno} dos={dos} tres={tres} cuatro={cuatro} cinco={cinco} seis={seis} />);
-    }
-}, 1000);
+        if (contador < 300) {
+            contador++;
+            containerApp.render(<SecondsCounter uno={uno} dos={dos} tres={tres} cuatro={cuatro} cinco={cinco} seis={seis} />);
+        }
+    }, 1000);
+};
 
-//render your react application
+window.onload = startCounter;
