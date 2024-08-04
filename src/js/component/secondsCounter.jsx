4bf476ba6c7f30d3a947db938062791e569d8345
Natalia Iacono <natalia.iacono.pr@gmail.com>
2024-08-04 13:26:56 +0200

    contador ok responsive 2.0

--- src/js/component/secondsCounter.jsx
+++ src/js/component/secondsCounter.jsx
@@ -7,12 +7,12 @@ const SecondsCounter = ({ uno, dos, tres, cuatro, cinco, seis }) => {
             <div className="icono col-12 col-sm-12 col-lg-2">
                 <i className="fa fa-clock text-white"></i>
             </div>
-            <div className="seis col-xs-4 col-sm-4 col-lg-1 text-white">{seis % 10}</div>
-            <div className="cinco col-xs-4 col-sm-4 col-lg-1 text-white">{cinco % 10}</div>
-            <div className="cuatro col-xs-4 col-sm-4 col-lg-1 text-white">{cuatro % 10}</div>
-            <div className="tres col-xs-4 col-sm-4 col-lg-1 text-white">{tres % 10}</div>
-            <div className="dos col-xs-4 col-sm-4 col-lg-1 text-white">{dos % 10}</div>
-            <div className="uno col-xs-4 col-sm-4 col-lg-1 text-white">{uno % 10}</div>
+            <div className="seis col-4 col-sm-4 col-lg-1 text-white">{seis % 10}</div>
+            <div className="cinco col-4 col-sm-4 col-lg-1 text-white">{cinco % 10}</div>
+            <div className="cuatro col-4 col-sm-4 col-lg-1 text-white">{cuatro % 10}</div>
+            <div className="tres col-4 col-sm-4 col-lg-1 text-white">{tres % 10}</div>
+            <div className="dos col-4 col-sm-4 col-lg-1 text-white">{dos % 10}</div>
+            <div className="uno col-4 col-sm-4 col-lg-1 text-white">{uno % 10}</div>
         </div>
     );
 };
