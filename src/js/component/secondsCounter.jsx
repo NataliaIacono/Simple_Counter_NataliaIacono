f762b181dee2cb1ec78e74c9f3a0f5ad68767c99
Natalia Iacono <natalia.iacono.pr@gmail.com>
2024-08-04 13:13:01 +0200

    responsive1

--- src/js/component/secondsCounter.jsx
+++ src/js/component/secondsCounter.jsx
@@ -4,15 +4,15 @@ import '../../styles/secondsCounter.css';
 const SecondsCounter = ({ uno, dos, tres, cuatro, cinco, seis }) => {
     return (
         <div className="container row">
-            <div className="icono col-lg-2 col-sm-12">
+            <div className="icono col-12 col-sm-12 col-lg-2">
                 <i className="fa fa-clock text-white"></i>
             </div>
-            <div className="seis col text-white">{seis % 10}</div>
-            <div className="cinco col text-white">{cinco % 10}</div>
-            <div className="cuatro col text-white">{cuatro % 10}</div>
-            <div className="tres col text-white">{tres % 10}</div>
-            <div className="dos col text-white">{dos % 10}</div>
-            <div className="uno col text-white">{uno % 10}</div>
+            <div className="seis col-xs-4 col-sm-4 col-lg-1 text-white">{seis % 10}</div>
+            <div className="cinco col-xs-4 col-sm-4 col-lg-1 text-white">{cinco % 10}</div>
+            <div className="cuatro col-xs-4 col-sm-4 col-lg-1 text-white">{cuatro % 10}</div>
+            <div className="tres col-xs-4 col-sm-4 col-lg-1 text-white">{tres % 10}</div>
+            <div className="dos col-xs-4 col-sm-4 col-lg-1 text-white">{dos % 10}</div>
+            <div className="uno col-xs-4 col-sm-4 col-lg-1 text-white">{uno % 10}</div>
         </div>
     );
 };
